7d03e306a07faf6b357a60d82784cca0c703f501
Howe251 <misha.sosnin245@gmail.com>
2021-10-10 18:04:42 +0300

    Заполнение массива размерности n рандомными числами и сортировка методом выбора

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,8 +1,38 @@
 ﻿#include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
-    cout << "TEST GIT"
+    setlocale(LC_ALL, "Russian");
+    srand(clock());
+    int size;
+    cout << "Введите размер массива: ";
+    cin >> size;
+
+    int* values = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        values[i] = rand();
+    }
+    cout << "Исходный массив:" << "\n";
+    for (int i = 0; i < size; i++) {
+        cout << i+1 << ": " << values[i] << "\n";
+    }
+    //Сортировка
+    for (int stInd = 0; stInd < size - 1; stInd++)
+    {
+        int maxIndex = stInd;
+        for (int curInd = stInd + 1; curInd < size; curInd++)
+        {
+            if (values[curInd] > values[maxIndex])
+                maxIndex = curInd;
+        }
+        swap(values[stInd], values[maxIndex]);
+    }
+    cout << "Отсортированный массив:" << "\n";
+    for (int i = 0; i < size; i++) {
+        cout << i + 1 << ": " << values[i] << "\n";
+    }
 }
